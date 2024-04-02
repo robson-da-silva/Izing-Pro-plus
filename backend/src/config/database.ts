aeb4dfafbaf0072b4e4f46064f2b14958202cad4
Douglas Fernandes <douglas@faroti.com.br>
2024-04-02 13:46:16 -0300

    Correção time zone e limitações do postgres

--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -6,12 +6,14 @@ module.exports = {
     collate: "utf8mb4_bin"
     // freezeTableName: true
   },
-  // pool: {
-  //   max: 100,
-  //   min: 5
-  // },
+  pool: {
+    max: 100,
+    min: 10,
+    acquire: 30000,
+    idle:  10000
+  },
   dialect: process.env.DB_DIALECT || "postgres",
-  timezone: "UTC",
+  timezone:  process.env.DB_TIMEZONE || "-03:00",
   host: process.env.POSTGRES_HOST || "localhost",
   port: process.env.DB_PORT || "5432",
   database: process.env.POSTGRES_DB || "wchats",
